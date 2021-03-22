5e7c6aae102201365252d1e4545f62b6a2b7d88d
Stoqn Tomicin <stoqntomicin@gmail.com>
2021-03-22 17:56:54 +0200

    Creating initial project structure

---
+++ financeTracker/src/test/java/financeTracker/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package financeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
