aacba4fe21b5978d99adcd7fd6ebd75ed9c3fcd8
Stoqn Tomicin <stoqntomicin@gmail.com>
2021-03-28 17:15:53 +0300

    Added category image service

--- financeTracker/src/main/java/financeTracker/controllers/CategoryImageController.java
+++ financeTracker/src/main/java/financeTracker/controllers/CategoryImageController.java
@@ -2,36 +2,27 @@ package financeTracker.controllers;
 
 import financeTracker.exceptions.NotFoundException;
 import financeTracker.models.dto.category_dto.CategoryImageDTO;
-import financeTracker.models.pojo.CategoryImage;
-import financeTracker.models.repository.CategoryImageRepository;
-import financeTracker.models.repository.CategoryRepository;
+import financeTracker.services.CategoryImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
-import java.nio.file.Files;
 
 @RestController
-public class ImageController {
+public class CategoryImageController {
     @Value("${file.path}")
     private String filePath;
     @Autowired
-    private CategoryRepository categoryRepository;
-    @Autowired
-    private CategoryImageRepository categoryImageRepository;
+    private CategoryImageService categoryImageService;
 
     @PutMapping("/category/{id}/images")
     public CategoryImageDTO upload(@PathVariable int id, @RequestPart MultipartFile file) {
         File pFile = new File(filePath + File.separator + System.nanoTime() + ".png");
         try (OutputStream os = new FileOutputStream(pFile)) {
             os.write(file.getBytes());
-            CategoryImage categoryImage = new CategoryImage();
-            categoryImage.setUrl(pFile.getAbsolutePath());
-            categoryImage.setCategory(categoryRepository.findById(id).get());
-            categoryImageRepository.save(categoryImage);
-            return new CategoryImageDTO(categoryImage);
+            return categoryImageService.upload(id, pFile);
         } catch (FileNotFoundException e) {
             throw new NotFoundException("File not found, reason - " + e.getMessage());
         } catch (IOException e) {
@@ -42,9 +33,6 @@ public class ImageController {
 
     @GetMapping(value = "images/{id}", produces = "image/*")
     public byte[] download(@PathVariable int id) throws IOException {
-        CategoryImage categoryImage = categoryImageRepository.findById(id).get();
-        String url = categoryImage.getUrl();
-        File pFile = new File(url);
-        return Files.readAllBytes(pFile.toPath());
+        return categoryImageService.download(id);
     }
 }
---
+++ financeTracker/src/main/java/financeTracker/services/CategoryImageService.java
@@ -0,0 +1,36 @@
+package financeTracker.services;
+
+import financeTracker.models.dto.category_dto.CategoryImageDTO;
+import financeTracker.models.pojo.CategoryImage;
+import financeTracker.models.repository.CategoryImageRepository;
+import financeTracker.models.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Service
+public class CategoryImageService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryImageRepository categoryImageRepository;
+
+
+    public CategoryImageDTO upload(int id, File pFile) {
+        CategoryImage categoryImage = new CategoryImage();
+        categoryImage.setUrl(pFile.getAbsolutePath());
+        categoryImage.setCategory(categoryRepository.findById(id).get());
+        categoryImageRepository.save(categoryImage);
+        return new CategoryImageDTO(categoryImage);
+    }
+
+    public byte[] download(int id) throws IOException {
+        CategoryImage categoryImage = categoryImageRepository.findById(id).get();
+        String url = categoryImage.getUrl();
+        File pFile = new File(url);
+        return Files.readAllBytes(pFile.toPath());
+    }
+}
